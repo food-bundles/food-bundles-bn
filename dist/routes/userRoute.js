aaa0c8b811b474dade7171f2ca2dc94d380371b9
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2025-09-17 14:27:52 +0200

    Delete dist folder

--- dist/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const userRoutes = (0, express_1.Router)();
-userRoutes.post("/login", userController_1.UserController.login);
-userRoutes.get("/me", userController_1.UserController.me);
-userRoutes.post("/logout", userController_1.UserController.logout);
-exports.default = userRoutes;
