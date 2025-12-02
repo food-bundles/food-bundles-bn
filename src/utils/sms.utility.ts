69647b0e2f51b55ada0549a063947867b17f13ee
EmmanuelSHYIRAMBERE <emashyirambere1@gmail.com>
2025-12-02 22:21:55 +0200

    Integrated Pindo as an SMS provider

--- src/utils/sms.utility.ts
+++ src/utils/sms.utility.ts
@@ -1,10 +1,9 @@
+import axios, { AxiosRequestConfig } from "axios";
 require("dotenv").config();
-const twilio = require("twilio")(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
 
-export const cleanPhoneNumber = (phone: string): string => {
+const token = process.env.SMS_API_TOKEN;
+
+export const cleanSMSPhoneNumber = (phone: string): string => {
   let cleaned = phone.replace(/\D/g, "");
   if (cleaned.startsWith("07")) {
     cleaned = "+2507" + cleaned.slice(2);
@@ -16,14 +15,34 @@ export const cleanPhoneNumber = (phone: string): string => {
 
 export async function sendMessage(messageBody: string, phoneNumber: string) {
   try {
-    const cleanedPhone = cleanPhoneNumber(phoneNumber);
-    const message = await twilio.messages.create({
-      body: `Hello from FoodBundles! ${messageBody}`,
-      from: process.env.TWILIO_TRIAL_PHONE_NUMBER,
+    const cleanedPhone = cleanSMSPhoneNumber(phoneNumber);
+
+    const data = {
       to: cleanedPhone,
-    });
-    console.log("SMS sent successfully:", message);
-    return message;
+      text: `Hello from FoodBundles! ${messageBody}`,
+      sender: process.env.SMS_SENDER_ID,
+    };
+
+    const options: AxiosRequestConfig = {
+      method: "POST",
+      url: "https://api.pindo.io/v1/sms/",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(options)
+      .then((response) => {
+        console.log({ message: "SMS message sent", data: response.data });
+
+        return response.data;
+      })
+      .catch((error) => {
+        console.error("SMS sending failed:", error);
+        throw error;
+      });
   } catch (error) {
     console.error("SMS sending failed:", error);
     throw error;
