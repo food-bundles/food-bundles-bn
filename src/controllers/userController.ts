208a009bff4e06d97768221bc1010c9301359705
MUVUNYI48 <devpatrick90@gmail.com>
2026-08-18 12:11:17 +0200

    error fixed from package-lock

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -595,6 +595,7 @@ export class UserController {
   };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   static myProfile = async (req: Request, res: Response) => {
     try {
       const authHeader = req.headers.authorization;
@@ -651,6 +652,8 @@ export class UserController {
 
 =======
 >>>>>>> dfd7b79 (error fixed from package-lock)
+=======
+>>>>>>> 7bc5b2e (error fixed from package-lock)
   static me = async (req: Request, res: Response) => {
     try {
       // Get token from Authorization header
