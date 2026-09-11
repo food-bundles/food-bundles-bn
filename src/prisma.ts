f6e53eba530427cb78148484952228169d645716
MUVUNYI48 <devpatrick90@gmail.com>
2026-09-11 13:22:34 +0200

    desc: changes on schema

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,13 +1,19 @@
-import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const DATABASE_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL_PROD
+    : process.env.DATABASE_URL_DEV;
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
